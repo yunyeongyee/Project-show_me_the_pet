4dec54b1b6c4e6530422307fd9a130c525309bbb
hi.its.yeey@gmail.com <hi.its.yeey@gmail.com>
2022-06-12 01:44:17 +0900

    UPDATE  DESIGN Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,79 @@
-import * as React from 'react';
+import React from 'react';
+import styled from 'styled-components';
 import {useNavigate} from 'react-router-dom';
+/*COMPONENTS*/
 /*CSS*/
-import './Header.css';
-import AppBar from '@mui/material/AppBar';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import '../App.css';
 
-const Header = () => {
+const Header = (props) => {
    const navigate = useNavigate();
-   const theme = createTheme();
+   const [isLogin, setIsLogin] = React.useState(false);
+   const [btnState, setBtnState] = React.useState();
+
+   const loginCheck = async (user) => {
+      if (user) {
+         setIsLogin(true);
+         setBtnState('LogOut');
+      } else {
+         setIsLogin(false);
+         setBtnState('Login');
+      }
+   };
+   // React.useEffect(() => {
+   //    onAuthStateChanged(auth, loginCheck);
+   // }, []);
+
+   const ClickLogin = () => {
+      // signOut(auth);
+      navigate('/Login');
+   };
+
    return (
-      <ThemeProvider theme={theme}>
-         <CssBaseline />
-         <AppBar position="relative">
-            <Toolbar className="Toolbar">
-               <h2 className="Typography">
-                  쇼미더펫
-               </h2>
-               <button className="BtnLogout">로그아웃</button>
-            </Toolbar>
-         </AppBar>
-      </ThemeProvider>
+      <>
+         <header className="Header">
+            <h1
+               className="HeaderTitle"
+               onClick={() => {
+                  window.location.replace('/');
+               }}
+            >
+               MAGAZINE
+            </h1>
+            <nav>
+               <ButtonLogIn onClick={ClickLogin}>{btnState}</ButtonLogIn>
+            </nav>
+         </header>
+      </>
    );
-}
+};
+const ButtonLogIn = styled.button`
+   width: 67px;
+   height: 27px;
+   float: right;
+   margin: 20px 10px;
+   padding: 5px;
+   align-items: center;
+   justify-content: center;
+   background-color: transparent;
+   border: 1px solid #61dafb;
+   border-radius: 5px;
+   color: #61dafb;
+   @keyframes push {
+      50% {
+         transform: scale(0.85);
+      }
+      100% {
+         transform: scale(1);
+      }
+   }
+   &:hover,
+   &:active,
+   &:focus {
+      cursor: pointer;
+      animation-name: push;
+      animation-duration: 0.4s;
+      animation-timing-function: linear;
+      animation-iteration-count: 1;
+   }
+`;
 export default Header;
