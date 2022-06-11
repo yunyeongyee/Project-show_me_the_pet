10049ac25aabdff9ef1383635b6e1e44144844d4
hi.its.yeey@gmail.com <hi.its.yeey@gmail.com>
2022-06-12 00:49:09 +0900

    UPDATE UploadBtn

--- src/components/UploadBtn.js
+++ src/components/UploadBtn.js
@@ -2,17 +2,13 @@ import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
 /*CSS*/
 import '../App.css';
-<<<<<<< HEAD
-const UploadBtn = () => {
-   const navigate = useNavigate();
-   return (
-=======
+
 
 const UploadBtn = () => {
    const navigate = useNavigate();
 
    return (  
->>>>>>> post
+
    <footer className="Footer">
           <button
              className="AddBtn"
@@ -22,14 +18,9 @@ const UploadBtn = () => {
              sx={{ fontSize: 70 }}
           >Add</button>
        </footer>
-<<<<<<< HEAD
-   );
-};
-export default UploadBtn;
-=======
 
 
    );
 };
 export default UploadBtn;
->>>>>>> post
+
