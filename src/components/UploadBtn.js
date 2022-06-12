bfed27fc5a5e9c89fafc4531c6698ad2bf268248
hi.its.yeey@gmail.com <hi.its.yeey@gmail.com>
2022-06-12 12:42:51 +0900

    UPDATE fontawesomeIcon

--- src/components/UploadBtn.js
+++ src/components/UploadBtn.js
@@ -1,18 +1,20 @@
 import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCloudPlus } from '@fortawesome/free-solid-svg-icons';
 /*CSS*/
 import '../App.css';
 const UploadBtn = () => {
    const navigate = useNavigate();
    return (
    <footer className="Footer">
-          <button
-             className="AddBtn"
+          
+ 
+ 
+<FontAwesomeIcon icon="faRegular faCloudPlus"  className="AddBtn"
              onClick={()=> {
                 window.location.replace('/AddPost');
-             }}
-             sx={{ fontSize: 70 }}
-          >Add</button>
+             }}/>
        </footer>
    );
 };
